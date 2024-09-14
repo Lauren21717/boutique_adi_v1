85dd89c267b55613db14821cbaa65369c5a9491e
Lauren Yeung <laurenyeung217@gmail.com>
2024-09-14 21:11:42 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Pyc4YRuJYd0ybghKYzwJZxOSsk1TVyl23x4VTJmro4GWu5nLeoMUDcY2pD5v9xhcziCnSMfeXAStXA5iJkcy4Up00woGqXbFb',
+        'client_serect': 'test client serect',
     }
 
     return render(request, template, context)
