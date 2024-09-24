5205d1795eb0b4bfb540f0d96b39990fb45695f4
Lauren Yeung <laurenyeung217@gmail.com>
2024-09-24 16:08:59 +0000

    Fix: varification issues

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -223,7 +223,7 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+# ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
